e36b25add8ffaf18fd359b437eec73a9054e5ade
Жабин Антон <zhabinal@zhabinal-mbp.local>
2022-10-06 20:08:24 +0300

    I wrote some tests to figure out how it works

--- Unit 4. Autotests/Kolobok_test.py
+++ Unit 4. Autotests/Kolobok_test.py
@@ -18,4 +18,25 @@ def text(change_test_dir):
 
 
 def test_has_header(text):
-    assert '# Сказка про колобка' in text
+    assert '# Сказка про Колобка' in text
+
+def test_has_subheader_kolobok_appeared(text):
+    assert '## 1. Как Колобок появился' in text
+
+def test_has_subheader_kolobok_runaway(text):
+    assert '## 2. Как Колобок убежал' in text
+
+def test_has_subheader_kolobok_faced_animals(text):
+    assert '## 3. Встреча со зверями' in text
+
+def test_has_subheader_kolobok_faced_hare(text):
+    assert '### 3.1 Заяц' in text
+
+def test_has_subheader_kolobok_faced_wolf(text):
+    assert '### 3.2 Серый волк' in text
+
+def test_has_subheader_kolobok_faced_bear(text):
+    assert '### 3.4 Медведь' in text
+
+def test_has_subheader_kolobok_faced_fox(text):
+    assert '## 4. Финал. Встреча с Лисой' in text
